1cefc1c9867e585c04388ebce6ff393633ec54ca
Caroline Moran <carolvandrade@gmail.com>
2023-12-02 10:02:54 -0400

    feat: footer

---
+++ layout/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import { Header } from "../../Components/medioComponents/header/Header";
+// import { Footer } from "../../Components/medioComponents/footer/Footer";
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* <Header /> */}
+      {/* <Footer /> */}
+    </footer>
+  );
+};
+
+export { Footer };

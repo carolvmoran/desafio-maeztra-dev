7940a68f5a2593d1d2f93f4f1fbedd81dd6f2f0c
Caroline Moran <carolvandrade@gmail.com>
2023-12-02 10:24:39 -0400

    feat: topBar

--- layout/src/components/Header/TopBar/TopBar.tsx
+++ layout/src/components/Header/TopBar/TopBar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./topBar.scss";
 
 // import { TopBar } from "../../Components/medioComponents/header/Header";
 // import { Footer } from "../../Components/medioComponents/footer/Footer";
 
 const TopBar = () => {
   return (
-    <div className="top-bar-container">
-      <p className="top-bar-text">
+    <div className="top-bar__container">
+      <p className="top-bar__text">
         Acompanhe as melhores promoções disponíveis aqui na Maeztra.
       </p>
     </div>

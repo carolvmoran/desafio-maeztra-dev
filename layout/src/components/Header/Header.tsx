1cee7137cb3ccf09490d91402a9032dd159384eb
Caroline Moran <carolvandrade@gmail.com>
2023-12-02 10:02:43 -0400

    feat: header

---
+++ layout/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { TopBar } from "./TopBar/TopBar";
+// import { Footer } from "../../Components/medioComponents/footer/Footer";
+
+const Header = () => {
+  return (
+    <header>
+      <TopBar />
+      {/* <Footer /> */}
+    </header>
+  );
+};
+
+export { Header };
---
+++ layout/src/components/Header/TopBar/TopBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import { TopBar } from "../../Components/medioComponents/header/Header";
+// import { Footer } from "../../Components/medioComponents/footer/Footer";
+
+const TopBar = () => {
+  return (
+    <div className="top-bar-container">
+      <p className="top-bar-text">
+        Acompanhe as melhores promoções disponíveis aqui na Maeztra.
+      </p>
+    </div>
+  );
+};
+
+export { TopBar };

f0175ed26481c5061a678eb6584d57377d313ec1
Caroline Moran <carolvandrade@gmail.com>
2023-12-02 22:01:53 -0400

    feat: carousel product card

--- layout/src/pages/Home.tsx
+++ layout/src/pages/Home.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
+import Carousel from "../components/Banners/Carousel";
+import CarouselProductCards from "../components/ProductCards/CarouselProductCards";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Carousel />
+        <CarouselProductCards />
+      </main>
       <Footer />
     </>
   );
